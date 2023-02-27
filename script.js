3502aae893d0d173cd131c5fef6401cf9dcd2afd
zacharydserafin <zacharydserafin@gmail.com>
2023-02-27 16:33:54 -0600

    added current time

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var currentDayEl = $("#currentDay")
+
+var today = dayjs();
+
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,5 +23,12 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
+  function displayTime() {
+    var todayFormat = dayjs().format("MM/DD/YYYY - hh:mm:ss a");
+    currentDayEl.text(todayFormat);
+  }
+
+  displayTime();
+  setInterval(displayTime, 1000);
 });
